2db02573e4f6a294342492fc35f57baa3362bc6c
abaudet <abaudet@sii.fr>
2018-03-30 22:12:17 +0200

    :question: Try to use webmail tab

---
+++ purejava-email-smtp/code/src/main/java/FirstEmail.java
@@ -0,0 +1,5 @@
+public class FirstEmail {
+  public static void main(String[] args) {
+    // Add your code here
+  }
+}
--- purejava-email-smtp/env-init.sh
+++ purejava-email-smtp/env-init.sh
@@ -1,5 +1,31 @@
-git clone https://github.com/groupe-sii/ogham.git
-cd sample-standard-usage
-mvn install -DskipTests
+#docker-compose up -d
+LOGFILE=/var/log/ogham-katacoda-debug.log
 
-docker pull roundcube/roundcubemail:1.3.4
+echo "" > $LOGFILE 2>&1
+
+# install dependencies
+apt update >> $LOGFILE 2>&1
+apt install -y git openjdk-8-jdk maven apache2 roundcube roundcube-sqlite3 >> $LOGFILE 2>&1
+echo "---------------------------------------------------------" >> $LOGFILE 2>&1
+
+service apache2 start >> $LOGFILE 2>&1
+
+cd ~/code
+
+# # download sample code
+# cd ~
+# git clone -b docs/katacoda https://github.com/groupe-sii/ogham.git >> $LOGFILE 2>&1
+# cd ogham/sample-standard-usage
+# ./mvnw install -DskipTests >> $LOGFILE 2>&1
+
+
+# # prepare editor sources
+# cd ~/tutorial
+# ln -s ~/ogham/sample-standard-usage
+
+
+
+#docker run -p 143:143 -p 587:587 -p 80:80 -d roundcube/roundcubemail:1.3.4 > $LOGFILE 2>&1
+
+
+
