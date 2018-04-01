c5cdca69070e78c6cb8e94fe7bce45151f29e4e0
abaudet <abaudet@sii.fr>
2018-04-01 23:03:43 +0200

    :construction:(editor) Tutorial sources

--- purejava-email-smtp/assets/prepare-env/display-install-progress.sh
+++ purejava-email-smtp/assets/prepare-env/display-install-progress.sh
@@ -1,5 +1,6 @@
 
 # TODO: display progress elsewhere in order to let students be able to write commands without need to press ctrl+c. Maybe another dedicated tab or a progressbar in JS with logs in the tooltip ?
 
+cd /home/tutorial
 #touch /var/log/ogham-katacoda-steps.log
 #tail -f /var/log/ogham-katacoda-steps.log
